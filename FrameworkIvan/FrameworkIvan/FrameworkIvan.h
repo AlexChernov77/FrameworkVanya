8420262c9cafa3a357406f35a07cb3ce1cc3fba7
Ivan Bogdanov <bogdanovis@inbox.ru>
2021-07-31 23:14:30 +0300

    Commit_1

---
+++ FrameworkIvan/FrameworkIvan/FrameworkIvan.h
@@ -0,0 +1,18 @@
+//
+//  FrameworkIvan.h
+//  FrameworkIvan
+//
+//  Created by Ivan on 31.07.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FrameworkIvan.
+FOUNDATION_EXPORT double FrameworkIvanVersionNumber;
+
+//! Project version string for FrameworkIvan.
+FOUNDATION_EXPORT const unsigned char FrameworkIvanVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameworkIvan/PublicHeader.h>
+
+
